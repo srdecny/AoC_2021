544f9f8454387896cfd85ddd05e45b3a3c8969af
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2021-12-01 09:09:43 +0100

    Day 1

--- src/utils.ts
+++ src/utils.ts
@@ -10,6 +10,14 @@ export const range = (start: number, end: number): number[] => {
     } else return range(end, start);
 }
 
+export const eachCons = <T>(n: number, arr: T[]): T[][] => {
+		const result: T[][] = [];
+		for (let i = 0; i < (arr.length - n + 1); i++) {
+				result.push([...arr.slice(i, i + n)]);
+		}
+		return result;
+}
+
 interface TestCase {
 	"input": string;
 	"expected" : string;
@@ -19,7 +27,8 @@ export const runTests = (testFile: string, part1: (input: string) => string, par
 		const testSamples: Record<string, [TestCase]> = JSON.parse(readFileSync(testFile, "utf8"));
 	["part1", "part2"].forEach(part => {
 			testSamples[part].forEach(test => {
-				const result = part === "part1" ? part1(test.input) : part2(test.input);
+				const testInput = readFileSync(test.input, "utf8");
+				const result = part === "part1" ? part1(testInput) : part2(testInput);
 				if (result !== test.expected) {
 					console.log(`Test failed for ${part}: ${test.input}`);
 					console.log(`Expected: ${test.expected}`);
