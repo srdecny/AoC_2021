f257b1c618bccf0d68116e5d19957da16b3d1928
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2021-12-13 13:24:55 +0100

    Day 13 pt2

--- src/13/main.ts
+++ src/13/main.ts
@@ -18,6 +18,10 @@ function parse(input: string): [Points, Fold[]] {
 		acc[y].push(x)
 		return acc
 	}, {} as Points)
+	const height = Math.max(...Object.keys(points).map(c => Number(c)))
+	for (let y = 0; y <= height; y++) {
+		if (!points[y]) points[y] = []
+	}
 
 	const folds = rawFolds.split("\n").map(c => {
 		const [dir, dist] = c.split(" ")[2].split("=");
@@ -28,28 +32,41 @@ function parse(input: string): [Points, Fold[]] {
 
 }
 
-function transpose(points: Points): Points {
-	const newPoints: Points = {};
-	for (const y in points) {
-		for (const x of points[y]) {
-			if (!newPoints[x]) newPoints[x] = []
-			newPoints[x].push(Number(y))
+
+
+function foldPaper(points: Points, fold: Fold): Points {
+	const [direction, line] = fold;
+	const newPoints: Points = {}
+	if (direction === DIRECTION.UP) {
+		for (let i = 1; i <= line; i++) {
+			const mergedPoints = [...new Set([...points[line + i] || [], ...points[line - i] || []])]
+			newPoints[line - i] = mergedPoints
 		}
+	} else {
+		Object.entries(points).forEach(([key, row]) => {
+			const mergedPoints = []
+			for (let i = 0; i <= line; i++) {
+				if (row.includes(line - i) || row.includes(line + i)) mergedPoints.push(line - i)
+			}
+			newPoints[Number(key)] = [...new Set(mergedPoints)]
+		})
 	}
 	return newPoints
 }
 
-function foldPaper(points: Points, fold: Fold): Points {
-	const [direction, line] = fold;
-	if (direction === DIRECTION.LEFT) points = transpose(points);
-	const newPoints: Points = {}
-	for (let i = 0; i <= line; i++) {
-		const mergedPoints = [...new Set([...points[line + i] || [], ...points[line - i] || []])]
-		newPoints[line - i] = mergedPoints
+function printPoints(points: Points) {
+	const maxLength = Math.max(...Object.values(points).map(c => Math.max(...c)))
+	const maxWidth = Math.max(...Object.keys(points).map(c => Number(c)))
+	for (let i = 0; i <= maxWidth; i++) {
+		for (let j = 0; j <= maxLength; j++) {
+			if (points[i] && points[i].includes(j)) {
+				process.stdout.write("#")
+			} else {
+				process.stdout.write(" ")
+			}
+		}
+		process.stdout.write("\n")
 	}
-	return newPoints
-
-
 }
 
 function part1(input: string): number {
@@ -61,23 +78,13 @@ function part1(input: string): number {
 function part2(input: string): string|number {
 	const [points, folds] = parse(input);
 	let finalPoints = folds.reduce((acc, c) => foldPaper(acc, c), points);
-	const maxLength = Math.max(...Object.values(finalPoints).map(c => Math.max(...c)))
-	const maxWidth = Math.max(...Object.keys(finalPoints).map(c => Number(c)))
-	for (let i = 0; i <= maxWidth; i++) {
-		for (let j = 0; j <= maxLength; j++) {
-			if (finalPoints[i] && finalPoints[i].includes(j)) {
-				process.stdout.write("#")
-			} else {
-				process.stdout.write(" ")
-			}
-		}
-		process.stdout.write("\n")
-	}
+	printPoints(finalPoints)
+
 	return 0
 }
 
 
-// runTests("tests.json", part1, part2);
+runTests("tests.json", part1, part2);
 const input = readFileSync("input.txt", "utf8").trim();
 console.log("Part 1: " + part1(input));
-console.log("Part 2: " + part2(input));
+console.log("Part 2: " + part2(input))
